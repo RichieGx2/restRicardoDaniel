53c4e80416323968f4f3f9c4fa2f17eab64dc25d
Ricardo García <richiesistemas@gmail.com>
2021-03-24 15:07:37 -0600

    Se hizo el ejercicio de asistencia

--- demorest/routes/index.js
+++ demorest/routes/index.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 //aqui definimos el endpoint (la url para acceder al recurso listarInvitados)
-const invitados= require("../modulos/invitados/guest");
-router.get('/v1/guest', invitados.listaInvitados)
+const invitados = require("../modulos/invitados/guest");
+router.get("/v1/guest", invitados.listaInvitados);
+
+const asistentes = require("../modulos/asistentes/status_assitance");
+router.get("/v1/asistencia", asistentes.listaStatus);
 module.exports = router;
